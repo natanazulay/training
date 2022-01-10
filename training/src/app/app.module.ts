c974bb6ab7ffe181f0c4c24eea3e85cd259f8ac9
MaorShrikiBehalf <maor.shriki@behlaf.com>
2022-01-10 21:32:53 +0200

    Maor Shriki - first session

--- training/src/app/app.module.ts
+++ training/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
+import { MatOptionModule } from "@angular/material/core";
+import { MatSelectModule } from "@angular/material/select";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChuckNorrisMainComponent } from "./chuck-norris-main/chuck-norris-main.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChuckNorrisMainComponent,
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
