2d0771588b8f9ed23f7c1f6b684a04c01d8162c7
lirbenshahar <lir.ben.shahar@behalf.com>
2022-01-17 19:54:50 +0200

    session 1 -correction 2

--- training/src/app/main/main.component.ts
+++ training/src/app/main/main.component.ts
@@ -6,16 +6,12 @@ import { JokeGeneratorService } from "../joke-generator.service";
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
+export class MainComponent {
   public joke: string = '';
 
   constructor(private jokeService: JokeGeneratorService) { }
 
-  ngOnInit(): void {
-        throw new Error('Method not implemented.');
-    }
-
-  showJoke(): void{
+  generateJoke(): void{
     this.jokeService.generateJoke().subscribe(joke => this.joke = joke.value);
   }
 }
