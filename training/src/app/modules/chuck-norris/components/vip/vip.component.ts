192ce32f57959906b76276abb7b96078f06c40c3
MaorShrikiBehalf <maor.shriki@behlaf.com>
2022-02-23 12:39:19 +0200

    Session 4 - Maor Shriki update

--- training/src/app/modules/chuck-norris/components/vip/vip.component.ts
+++ training/src/app/modules/chuck-norris/components/vip/vip.component.ts
@@ -8,16 +8,13 @@ import { ActivatedRoute } from "@angular/router";
 	styleUrls: ['./vip.component.scss']
 })
 export class VipComponent implements OnInit {
-	// @Input() public joke: Joke
+
 	public vipJoke: Joke;
 
 	constructor(private route: ActivatedRoute) {
 	}
 
 	public ngOnInit() {
-		// this.isSearchMode = this.route.snapshot.data[ 'isSearchMode' ];
-		// this.isVipMode    = this.route.snapshot.data[ 'isVipMode' ];
-		// this.isNotVipMode = this.route.snapshot.data[ 'isNotVipMode' ];
 		this.vipJoke = this.route.snapshot.data[ 'vipJoke' ];
 	}
 }
